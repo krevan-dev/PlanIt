8725a31ce66a9eccec13f68d42465d20c1c0484d
Kyle Evans <krevan88@protonmail.com>
2022-01-26 09:27:19 -0700

    Server is up and running we good homies

--- PlanIt.client/src/env.js
+++ PlanIt.client/src/env.js
@@ -2,5 +2,5 @@ export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
 export const useSockets = false
 export const domain = 'dev-hgqjrro9.us.auth0.com'
-export const clientId = 'https://gregslist.com'
-export const audience = 'JrqqXscucubFtUlHT7GxKeNrn0rvmBXv'
+export const clientId = 'JrqqXscucubFtUlHT7GxKeNrn0rvmBXv'
+export const audience = 'https://gregslist.com'
