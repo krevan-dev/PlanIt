e51465465df9e663c0698b177a104db5ef741f5b
Madison Yarnall <maddycodes01@gmail.com>
2022-01-26 13:57:05 -0700

    env

--- PlanIt/client/app/env.js
+++ PlanIt/client/app/env.js
@@ -1,5 +1,7 @@
 export const dev = window.location.origin.includes('localhost')
 export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
 export const domain = 'dev-hgqjrro9.us.auth0.com'
-export const audience = 'https://gregslist.com'
 export const clientId = 'JrqqXscucubFtUlHT7GxKeNrn0rvmBXv'
+export const audience = 'https://gregslist.com'
+
