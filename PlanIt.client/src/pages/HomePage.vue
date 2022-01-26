f23163ea162d00d88d35c04172e55b9c023c50db
Madison Yarnall <maddycodes01@gmail.com>
2022-01-26 15:41:42 -0700

    push

--- PlanIt.client/src/pages/HomePage.vue
+++ PlanIt.client/src/pages/HomePage.vue
@@ -1,23 +1,13 @@
 <template>
-  <div
-    class="
-      home
-      flex-grow-1
-      d-flex
-      flex-column
-      align-items-center
-      justify-content-center
-    "
-  >
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img
-        src="https://cdn.freelogodesign.org/files/cec092aaa70248eea0f51596bcf9f4ae/thumb/logo_200x200.png?v=0"
-        alt="CodeWorks Logo"
-        class="rounded-circle"
-      />
-      <h1 class="my-5 text-white p-3 rounded text-center signInCard">
-        Welcome to PlanIt. Please sign in to continue.
-      </h1>
+  <div class="container-fluid">
+    <div class="card m-4 projectcard">
+      <div class="card-body p-5">
+        <h5 class="card-title cardtitle">Project Name</h5>
+        <p class="card-text carddescription">
+          This is gonna be the super cool description of the project that we are
+          all super excited about.
+        </p>
+      </div>
     </div>
   </div>
 </template>
@@ -51,5 +41,15 @@ export default {
 .signInCard {
   background-color: #6b1592;
   font-family: "Dosis", sans-serif;
+}
+
+.projectcard {
+  border-color: #8900ed;
+  border-width: 5px;
+}
+
+.cardtitle {
+  font-family: "Dosis", sans-serif;
+  font-weight: 500;
 }
 </style>
