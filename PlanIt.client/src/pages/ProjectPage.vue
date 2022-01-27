7ae74b3a692453d3db2d95ce93683eddca426f2c
Kyle Evans <krevan88@protonmail.com>
2022-01-27 13:32:45 -0700

    push it all push it real good

--- PlanIt.client/src/pages/ProjectPage.vue
+++ PlanIt.client/src/pages/ProjectPage.vue
@@ -25,68 +25,10 @@
           <div>
             <p class="sprintstag mx-3"><b>Sprints</b></p>
           </div>
-          <div class="createtaskmodal">
-            <!-- Button trigger modal -->
-            <button
-              type="button"
-              class="btn btn-outline-info"
-              data-bs-toggle="modal"
-              data-bs-target="#exampleModal"
-            >
-              Create New Task +
-            </button>
-
-            <!-- Modal -->
-            <div
-              class="modal fade"
-              id="exampleModal"
-              tabindex="-1"
-              aria-labelledby="exampleModalLabel"
-              aria-hidden="true"
-            >
-              <div class="modal-dialog">
-                <div class="modal-content">
-                  <div class="modal-header">
-                    <h5 class="modal-title text-light" id="exampleModalLabel">
-                      Create a New Task!
-                    </h5>
-                    <button
-                      type="button"
-                      class="btn-close"
-                      data-bs-dismiss="modal"
-                      aria-label="Close"
-                    ></button>
-                  </div>
-                  <div class="modal-body">
-                    <form>
-                      <p>What do I need to do today?</p>
-                      <input
-                        type="text"
-                        placeholder="Task..."
-                        required="true"
-                      />
-                      <p>How hard is this task?</p>
-                      <input type="number" min="1" max="10" required="true" />
-                    </form>
-                  </div>
-                  <div class="modal-footer">
-                    <button
-                      type="button"
-                      class="btn btn-secondary"
-                      data-bs-dismiss="modal"
-                    >
-                      Close
-                    </button>
-                    <button type="button" class="btn btn-info">Add Task</button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
       <div class="col-md-12">
-        <Sprint v-for="s in sprints" :key="s.id" :sprint="s"/>
+        <Sprint v-for="s in sprints" :key="s.id" :sprint="s" class="mb-2"/>
       </div>
     </div>
   </div>
---
+++ PlanIt.client/src/services/TasksService.js
@@ -0,0 +1,19 @@
+class TasksService {
+  async getTasks() {
+
+  }
+
+  async createTask() {
+
+  }
+
+  async editTask() {
+
+  }
+
+  async deleteTask() {
+
+  }
+}
+
+export const tasksService = new TasksService
