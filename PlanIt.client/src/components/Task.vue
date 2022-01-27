e0eca272602d12f2f4052e37fc83a595f5d6a30f
Kyle Evans <krevan88@protonmail.com>
2022-01-27 13:57:07 -0700

    mark sync

---
+++ PlanIt.client/src/components/Task.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="component">
+    <!-- <p>{{ task.name }}<i class="mdi mdi-weight"></i>{{ task.weight }}</p> -->
+
+  </div>
+</template>
+
+
+<script>
+export default {
+  setup(){
+    return {}
+  }
+}
+</script>
+
+
+<style lang="scss" scoped>
+
+</style>
